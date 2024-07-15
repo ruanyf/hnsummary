8eb3791e97dbaeffbe59d1d9c2e3962ac393056f
ruanyf <yifeng.ruan@gmail.com>
2024-07-15 19:04:51 +0800

    refactor: add debug output

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const response = await fetch('https://hackyournews.com');
 const responseBody = await response.text();
 
 console.log('finish fetching');
-console.log(responseBody);
+
 
 const responseRoot = parse(responseBody);
 const trs = responseRoot.querySelectorAll('tr');
+
+console.log({ trs });
 
 if (!trs.length) process.exit();
 
